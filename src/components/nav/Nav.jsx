49c11f8ac61f55a34430ef3c7aefa2b6c1b4fab2
Luis Alejandro Peña Rivas <luisrivas0311@gmail.com>
2023-12-06 22:10:49 -0300

    arregl9o front365   arregtlo nav23dd3eefdfxxxd

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,69 +1,142 @@
-import React from 'react'
-import logo from '../../asset/img/logo_new_2.jpg'
-import './Nav.css'
-import { NavLink } from 'react-router-dom'
 
+import React from 'react';
+import logo from '../../asset/img/logo_new_2.jpg';
+import './Nav.css';
+import { NavLink } from 'react-router-dom';
 
 export default function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light allegory-nav__container">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="allegory-nav__links--left navbar-nav">
+          <NavLink activeClassName="active" exact to="/Home" className="nav-link">
+            Inicio
+          </NavLink>
+          <NavLink activeClassName="active" exact to="/buscador" className="nav-link">
+            Buscador
+          </NavLink>
+          <NavLink activeClassName="active" exact to="/nosotras" className="nav-link">
+            Nosotras
+          </NavLink>
+          <NavLink activeClassName="active" exact to="/blog" className="nav-link">
+            Blog
+          </NavLink>
+        </div>
+        <div className="col-6 allegory-nav__logo">
+          <a>
+            <img src={logo} alt="Logo de la página" />
+          </a>
+        </div>
+        <div className="col-2">
+          <div type="button" className="text-right btn btn-calipso">
+            <NavLink activeClassName="active" exact to="/register" className="nav-link">
+              Suscríbete
+            </NavLink>
+          </div>
+        </div>
+        <div className="col-1">
+          <div type="button" className="text-right btn btn-calipso">
+            <NavLink activeClassName="active" exact to="/login" className="nav-link">
+              Iniciar sesión
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import logo from '../../asset/img/logo_new_2.jpg'
+// import './Nav.css'
+// import { NavLink } from 'react-router-dom'
+
+
+// export default function Nav() {
 
  
 
   
 
 
-  return (
+//   return (
     
 
-    <nav className="navbar navbar-expand-lg navbar-light allegory-nav__container">
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse allegory-nav__links col-3" id="navbarNav">
-      <div className="allegory-nav__links--left">
-        <NavLink activeClassName="active" exact to="/Home">
-          Inicio
-        </NavLink>
-        <NavLink activeClassName="active" exact to="/buscador">
-          Buscador
-        </NavLink>
-        <NavLink activeClassName="active" exact to="/nosotras">
-          Nosotras
-        </NavLink>
-        <NavLink activeClassName="active" exact to="/blog">
-          Blog
-        </NavLink>
-      </div>
-    </div>
-    <div className="col-6 allegory-nav__logo">
-      <a>
-        <img src={logo} alt="Logo de la página" />
-      </a>
-    </div>
-    <div className="col-2">
-      <div type="button" className="text-right btn btn-calipso">
-        <NavLink activeClassName="active" exact to="/register">
-          Suscríbete
-        </NavLink>
-      </div>
-    </div>
-    <div className="col-1">
-      <div type="button" className="text-right btn btn-calipso">
-        <NavLink activeClassName="active" exact to="/login">
-          Iniciar sesión
-        </NavLink>
-      </div>
-    </div>
-  </nav>
-);
-}
+//     <nav className="navbar navbar-expand-lg navbar-light allegory-nav__container">
+//     <button
+//       className="navbar-toggler"
+//       type="button"
+//       data-toggle="collapse"
+//       data-target="#navbarNav"
+//       aria-controls="navbarNav"
+//       aria-expanded="false"
+//       aria-label="Toggle navigation"
+//     >
+//       <span className="navbar-toggler-icon"></span>
+//     </button>
+//     <div className="collapse navbar-collapse allegory-nav__links col-3" id="navbarNav">
+//       <div className="allegory-nav__links--left">
+//         <NavLink activeClassName="active" exact to="/Home">
+//           Inicio
+//         </NavLink>
+//         <NavLink activeClassName="active" exact to="/buscador">
+//           Buscador
+//         </NavLink>
+//         <NavLink activeClassName="active" exact to="/nosotras">
+//           Nosotras
+//         </NavLink>
+//         <NavLink activeClassName="active" exact to="/blog">
+//           Blog
+//         </NavLink>
+//       </div>
+//     </div>
+//     <div className="col-6 allegory-nav__logo">
+//       <a>
+//         <img src={logo} alt="Logo de la página" />
+//       </a>
+//     </div>
+//     <div className="col-2">
+//       <div type="button" className="text-right btn btn-calipso">
+//         <NavLink activeClassName="active" exact to="/register">
+//           Suscríbete
+//         </NavLink>
+//       </div>
+//     </div>
+//     <div className="col-1">
+//       <div type="button" className="text-right btn btn-calipso">
+//         <NavLink activeClassName="active" exact to="/login">
+//           Iniciar sesión
+//         </NavLink>
+//       </div>
+//     </div>
+//   </nav>
+// );
+// }
 //     <nav className="navbar navbar-expand-lg navbar-light allegory-nav__container">
 //     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 //         <span className="navbar-toggler-icon"></span>
